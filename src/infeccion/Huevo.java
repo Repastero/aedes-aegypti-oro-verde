698e0cc912903d6c554e51d94beb4f97272b3725
Repastero <alt.f3-3e1gsud@speed.1s.fr>
2019-08-01 19:32:09 -0300

    Cambios generales.
    Partes de codigo en groovy pasadas a java.
    Limpieza, formateo de codigo y nuevos comentarios.
    Eliminado de la clase el array de cantidades de Huevos y su manipulacion.
    El Container a cual pertenece el Huevo, se asigna directamente en el constructor.
    Eliminada la funcion "restrainAmountEggs" ya que ahora no hay forma que los Containers sobrepaseen la capacidad de acarreo.
    Eliminada la funcion "getDesarrolloMetabolicodiario" ya que ahora se calcula una sola vez para todos los Huevos en la clase Clima.

--- src/infeccion/Huevo.java
+++ src/infeccion/Huevo.java
@@ -1,168 +1,63 @@
-package infeccion
+package infeccion;
 
-import static java.lang.Math.*
-import static repast.simphony.essentials.RepastEssentials.*
+import infeccion.LifeCicle;
+import repast.simphony.ui.probe.*;
 
-import java.util.ArrayList;
-import java.util.Set
-
-import bsh.This
-import infeccion.LifeCicle
-import repast.simphony.engine.schedule.*
-import repast.simphony.parameter.*
-import repast.simphony.space.grid.Grid
-import repast.simphony.space.grid.GridPoint
-import repast.simphony.ui.probe.*
-
-public class StateHuevo extends LifeCicle{
+public class Huevo extends LifeCicle {
 	private static int agentIDCounter = 0;
-	public  static int setCantidad(int val){this.agentIDCounter = val;}
-	public  static int getCantidad(){return this.agentIDCounter;}
-	
-	private static ArrayList<Integer> 	     acuaticSet = new ArrayList<Integer>();
-	public  static ArrayList<Integer>      getAcuaticSet() {return this.acuaticSet}
-	public  static int 	          getAcuaticSettSize(){ return this.acuaticSet.size()}	//crear un metodo en WaterSites--> WaterSites.getAcuaticAmount(agentID)   //agentId = id del contenedor
-	public  static void 	    addToAcuaticSet(Integer newValue){this.acuaticSet.add(newValue)}
-	public  static boolean      containsAcuaticSet(Integer value){return this.acuaticSet.contains(value);}
-	public  static void 	   removeFromAcuaticSet(Integer value){
-		int index = this.acuaticSet.indexOf(value)
-			this.acuaticSet.remove(index);
-	}
-	
-	private double    initialWater;
+
+	private Container container; // para saber nivel del agua
+	private double initialWater;
 	private double getInitialWater() {
 		return initialWater;
 	}
-	private void   setInitialWater(double initialWater) {
-		this.initialWater = initialWater;
-	}
 	
-	
-	StateHuevo(){}//constructor vacio para generar archivo con cantidades por dia 
-	StateHuevo(GridPoint myPosition, Integer agentID){     
-		super(myPosition);
-		this.agentIDCounter += 1;
-		this.addToAcuaticSet(agentID);
-		this.inicializarInitialWater();
-		this.checkIfAddToEggsAmount()
-    }
+	public  static void setCantidad(int val) { agentIDCounter = val; }
+	public  static int getCantidad(){ return agentIDCounter; }
 
-	StateHuevo(double life, Integer agentID){
+	Huevo() { }//constructor vacio para generar archivo con cantidades por dia
+	Huevo(double life, Integer agentID, Container container) {
 		super(life);
-		this.agentIDCounter += 1;
-		this.addToAcuaticSet(agentID);
-		this.inicializarInitialWater();
-		this.checkIfAddToEggsAmount()
-	}
-	
-	private checkIfAddToEggsAmount() {
-		int dia = Clima.getDayNumber();
-		if(dia == 0){
-			Conteiner myConteiner = this.getMyConteiner()
-			if(myConteiner != null){
-				myConteiner.addOneToEggsAmount();
-			}
-		}
-	}
-	
-	private inicializarInitialWater() {
-		Conteiner myConteiner = this.getMyConteiner() 
-		if(myConteiner != null){
-			def mmWaterIniciales = myConteiner.getMmWater();
-			this.setInitialWater(mmWaterIniciales)
-		}
-	}
-	
-	public  void updateLife(){
-		double newLife = this.getLife() + this.getDesarrolloMetabolicodiario();
-		if(newLife >= 0.95){			
-			this.habilitarEclocion(newLife);
-		}
-		else{
-			this.setLife(newLife);
-		}
-	}
-	
-	public double getDesarrolloMetabolicodiario(){
-		double R   = 0.98588;
-		double Rdk = 0.24;
-		double Ha  = 10798;
-		double Hh  = 100000;
-		double T12 = 14184;
-		double desarrolloDiario = this.getDesarrolloMetabolicodiario(R, Rdk, Ha, Hh, T12);
-		return desarrolloDiario;
+		Huevo.agentIDCounter++;
+		this.container = container;
+		this.initialWater = container.getMmWater();
 	}
 
-	private void habilitarEclocion(double newLife){
-		double distanciaHuevoVsAgua = 5 // 5 [mm]//dataSet.getAedesAcuatico_Huevo_distanciaAguaVsHuevo();
-		Conteiner c = this.getMyConteiner(); 
-		if(c != null){
-			if(!c.getInside()){
-				if(c.getMmWater() > this.getInitialWater()){
-					this.setLife(newLife);
-				}
-			}else{
-				this.setLife(newLife);
-			}
-		}else{
-			this.setLife(newLife);
+	@Override
+	public void updateLife() {
+		double desMeta = Clima.getDMDHuevo();
+		super.updateLife(desMeta);
+		if (super.getLife() >= 0.95d)
+			habilitarEclocion();
+	}
+
+	/**
+	 * No permite eclosionar al Huevo hasta que el nivel de agua no supere el nivel inicial.<p>
+	 * Si el contenedor es intradomiciliario, el Huevo eclosiona al cumplir su desarrollo.
+	 */
+	private void habilitarEclocion() {
+		if (!container.isInside() && (container.getMmWater() <= getInitialWater())) {
+			super.setLife(0.94d);
 		}
 	 }
-	
-	public double mortalidadDiaria(){
-		double mortalidad = dataSet.getAedesacuatico_Huevo_Mortalidaddiaria();
-		return mortalidad;
+
+	@Override
+	protected double mortalidadDiaria() {
+		return DataSet.mortalidadDiariaHuevos;
 	}
-	
-	public void mortalidadNeta(){
-		double mortalidad = this.mortalidadDiaria();
-		def life = this.getLife();
-		if(this.restrainAmountEggs() && (life == 0)){ 
-			mortalidad = 1;
-		}
-		this.setMortalidadNeta(mortalidad);
+
+	@Override
+	public double updateMortalidadNeta() {
+		return mortalidadDiaria();
 	}
-	
-	public void eliminate(int agentID){
-		this.getMyConteiner().restOneToEggsAmount();
-		this.removeFromAcuaticSet(agentID)
-		this.agentIDCounter -= 1;
+
+	@Override
+	public void eliminate(int agentID) {
+		Huevo.agentIDCounter--;
 	}
-	
-	private boolean restrainAmountEggs(){
-		boolean restrain = false;
-		Conteiner myConteiner = this.getMyConteiner();
-		if(myConteiner != null){
-			int acuaticAmountInConteiner = myConteiner.getAcuaticAmount();
-			int ccEggs = this.getMyConteiner().getCarryingCapacityEggs();
-			if(acuaticAmountInConteiner > ccEggs){
-				restrain = true;
-			}
-		}
-		return restrain;
-	}
-	
-	@ProbeID()
+
+	@ProbeID
 	public String toString() {
-		def returnValue;
-		def time = GetTickCountInTimeUnits();
-		returnValue = this.agentID;
-		return returnValue;
+		return String.valueOf(Huevo.agentIDCounter);
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
