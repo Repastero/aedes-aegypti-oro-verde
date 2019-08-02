cd5fa1787d5790f9705948fdc1b5506d482ef9bd
Repastero <alt.f3-3e1gsud@speed.1s.fr>
2019-08-02 16:38:35 -0300

    Cambios generales.
    Partes de codigo en groovy pasadas a java.
    Limpieza, formateo de codigo y nuevos comentarios.
    Removidas varibles no utilizadas y sus metodos Getters y Setters.
    Removidos metodos que ya no son de utilidad para la clase.

--- src/infeccion/LifeCicle.java
+++ src/infeccion/LifeCicle.java
@@ -1,126 +1,58 @@
-package infeccion
+package infeccion;
 
-import static java.lang.Math.*
-import static repast.simphony.essentials.RepastEssentials.*
-import repast.simphony.engine.schedule.*
-import repast.simphony.parameter.*
-import repast.simphony.space.grid.Grid
-import repast.simphony.space.grid.GridPoint
-import repast.simphony.ui.probe.*
-
-public abstract class LifeCicle{ 
-	LifeCicle(){}
-	LifeCicle(GridPoint myPosition){
-		this.setLife(0);
-		this.setMyPosition(myPosition);
-	};
-	LifeCicle(double life){
-		this.setLife(life);
-	};
+public abstract class LifeCicle {
+	/** Porcentaje de desarrollo de la fase actual. */
+	private double life = 0f;
 	
-	//--------------------------------------Propertys-----------------------------------------------
-	private   double    life;
-	protected double getLife() {
-		return this.life;
-	}
-	protected void 	 setLife(double newValue) {
-		this.life = newValue;
-	}
-
-	private	double    mortalidadNeta;
-	public 	double getMortalidadNeta(){
-		this.mortalidadNeta()
-		return this.mortalidadNeta
-	};
-	public 	double setMortalidadNeta(double newValue){
-		this.mortalidadNeta = newValue
-	};
-	
-	private   boolean    eclocionHumana = false;											//comentar
-	protected boolean getEclocionHumana(){
-		return this.eclocionHumana;
-	}						//comentar
-	protected void 	  setEclocionHumana(boolean newValue){ 
-		this.eclocionHumana = newValue; 
-	}//comentar
-		
-	private GridPoint    myPosition;
-	public  GridPoint getMyPosition() {
-		return this.myPosition;
-	}
-	public  void 	  setMyPosition(GridPoint gridPoint) {
-		this.myPosition = gridPoint;
-	}
-	
-	private Conteiner    myConteiner;
-	public  Conteiner getMyConteiner(){
-		this.searchConteiner()
-		return this.myConteiner;
-	}
-	public  void 	  setMyConteiner(Conteiner newValue) {
-		this.myConteiner = newValue;
-	}
-
-	//--------------------------------------Methods-----------------------------------------------
+	/**
+	 * Incrementa porcentaje de desarrollo, de acuerdo a fase actual.
+	 */
 	public    abstract void updateLife();
 	
-	public 	  abstract void mortalidadNeta() //[0..1]metodo que devuelve la sumatorias de probabilidad de muertes (diaria+capacidad de acarreo).
+	/**
+	 * Sumatoria de mortalidades.
+	 * @return porcentaje de mortalidad
+	 */
+	public 	  abstract double updateMortalidadNeta();
 	
+	/**
+	 * Mortalidad diaria, de acuerdo a fase actual.
+	 * @return porcentaje de mortalidad
+	 */
 	protected abstract double mortalidadDiaria();
-
+	
+	/**
+	 * Resta el Agente de la cantidad total.
+	 * @param agentID ID del Agente
+	 */
 	public    abstract void eliminate(int agentID);
+	//
+	LifeCicle() { }
 	
-	protected double getDesarrolloMetabolicodiario(double R,double Rdk, double Ha, double Hh, double T12){
-		def T = Clima.getTemperatureDay() + 273;
-		def desarrolloMetabolicodiario = Rdk*(T/298)*Math.exp((Ha/R)*(1/298  - 1/T))   / (1 + Math.exp((Hh/R)*(1/T12  - 1/T)));
-		return desarrolloMetabolicodiario;
-	}
-
-	private void searchConteiner(){
-		//		if (this.myConteiner == null){
-		GridPoint pt = this.getMyPosition();
-		if(pt !=null){
-			Grid 	  grid    = FindGrid("infeccion/grid");
-			Iterable objects = grid.getObjectsAt(pt.getX(), pt.getY());
-			Conteiner c = this.myConteiner
-			while (objects.hasNext() && c == null) {
-				Object o = objects.next()
-				o instanceof Conteiner ? this.setMyConteiner(o) : {};
-			}
-		}
-		//		}
-	}
-		
-	public static void saveFileAmount(Class myType){
-		String path = 'C:/RepastSimphony-2.4/eclipse/WorkSapaces/Tesis/infeccion/OutFiles/';
-		String fileName= myType.getSimpleName() +'.txt';
-		String pathFileName = path + fileName;
-		File file = new File(pathFileName)
-		(!file.isFile()) ? this.generateFile(file, myType): {};
-		def dia = Clima.getDayNumber();
-		def diaLimite = 10;  //dia para almacenar corridas
-		if(dia == 1){
-			def name = myType.getSimpleName();
-			def runNumber = Clima.runNumber.toString();
-			def headVector = name + runNumber +" <- c("
-			file << headVector;
-		}
-		if(dia < diaLimite){
-			def a = myType.newInstance();
-			def cantidad = a.getCantidad();
-			String newLine = cantidad + ", ";
-			(dia == diaLimite - 1) ? newLine = cantidad : {} ;
-			file << newLine;
-		}
-		else{
-			dia == diaLimite ? file << ")\n\n" : {} ;
-		}
+	LifeCicle(double value) {
+		this.life = value;
+	};
+	
+	/**
+	 * @return Porcentaje de desarrollo de la fase actual.
+	 */
+	public double getLife() {
+		return life;
 	}
 	
-	private static void generateFile(File file, Class myType){
-		def name = myType.getSimpleName();
-		String newLine =  "\n";
-		file << newLine;
+	/**
+	 * Cambia el porcentaje de desarrollo de la fase actual.
+	 * @param value nuevo porcentaje de desarrollo
+	 */
+	public void setLife(double value) {
+		life = value;
+	}
+	
+	/**
+	 * Actualiza el porcentaje de desarrollo de la fase actual.
+	 * @param value porcentaje de desarrollo a sumar
+	 */
+	public void updateLife(double value) {
+		this.life += value;
 	}
 }
-
